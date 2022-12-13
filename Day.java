8c4e236dd3db0394f2fa402685b78f37e9241e88
Robel Endashaw <101689205+Dofivile1@users.noreply.github.com>
2022-12-12 22:10:03 -0500

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,24 @@
+
+/*
+ * Class: CMSC203 
+ * Instructor:
+ * Description: (Give a brief description for each Class)
+ * Due: MM/DD/YYYY
+ * Platform/compiler:
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Robel Endashaw
+*/
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+
+}
